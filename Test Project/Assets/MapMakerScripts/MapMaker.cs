73d879523731eb72532cf6d4292bf8e82ecd7ab6
Charles Mogan <charlesdmogan@gmail.com>
2017-08-22 06:11:13 -0400

    tat 5 working

--- Test Project/Assets/MapMakerScripts/MapMaker.cs
+++ Test Project/Assets/MapMakerScripts/MapMaker.cs
@@ -25,6 +25,13 @@ public class MapMaker : MonoBehaviour {
 	[Range(0,25)]
 	public int borderSize;
 
+	[Range(0,250)]
+	public int wallThresholdSize;
+
+	[Range(0,250)]
+	public int roomThresholdSize;
+	
+
 
 	private int [,] map;
 
@@ -48,8 +55,8 @@ public class MapMaker : MonoBehaviour {
 		for(int i = 1; i <= smoothingPasses; i++){
 			SmoothMapV2();
 		}
-
-
+		//from tatorial 5
+		removeSmallMapRegions();
 
 		//stuff from the second tutroial 
 		MeshGenerator meshGen = GetComponent<MeshGenerator>();
@@ -150,6 +157,88 @@ public class MapMaker : MonoBehaviour {
 		return numberOfWalls;
 	}
 
+
+
+
+	struct Coordinate{
+		public int x;
+		public int y;
+
+		public Coordinate(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+
+	List<Coordinate> GetRegonTiles(int startingX, int startingY){
+		List<Coordinate> tiles = new List<Coordinate>();
+		int[,] mapFlags = new int[width,height];
+		Queue<Coordinate> queue = new Queue<Coordinate>();
+		queue.Enqueue(new Coordinate(startingX,startingY));
+		mapFlags [startingX,startingY] = 1;
+		int tileType = map[startingX,startingY];
+
+		while(queue.Count > 0){ // maybe good
+			Coordinate tile = queue.Dequeue();
+			tiles.Add(tile);
+
+			for(int x = tile.x - 1; x <= tile.x + 1; x++){
+				for(int y = tile.y - 1; y <= tile.y + 1; y++){
+					if(IsInMapRange(x,y) && (y == tile.y || x == tile.x)){
+						if (mapFlags[x,y] == 0 && map[x,y] == tileType){
+							mapFlags[x,y] = 1;
+							queue.Enqueue(new Coordinate(x,y));
+						}
+					}
+				}
+			}
+		}
+		return tiles;
+	}
+
+	bool IsInMapRange(int x, int y){
+		return(x >= 0 && x < width && y >= 0 && y < height);
+	}
+
+
+	List<List<Coordinate>> GetRegions(int tileType){
+		List<List<Coordinate>> regions = new List<List<Coordinate>>();
+		int[,] mapFlags = new int[width,height];
+		for(int x = 0; x < width; x++){	
+			for(int y = 0; y < height; y++){
+				if(mapFlags[x,y] == 0 && map[x,y] == tileType){
+					List<Coordinate> newRegion = GetRegonTiles(x,y);
+					regions.Add(newRegion);
+					foreach(Coordinate tile in newRegion){
+						mapFlags[tile.x,tile.y] = 1;
+					}
+				}
+			}
+		}
+		return regions;
+	}
+
+	void removeSmallMapRegions(){
+		List<List<Coordinate>> wallRegions = GetRegions(1);
+		foreach(List<Coordinate> wallRegion in wallRegions){
+			if(wallRegion.Count < wallThresholdSize){
+				foreach(Coordinate tile in wallRegion){
+					map[tile.x,tile.y] = 0;
+				}
+			}
+		}
+
+
+		List<List<Coordinate>> roomRegions = GetRegions(0);
+		foreach(List<Coordinate> roomRegion in roomRegions){
+			if(roomRegion.Count < roomThresholdSize){
+				foreach(Coordinate tile in roomRegion){
+					map[tile.x,tile.y] = 1;
+				}
+			}
+		}
+	}
 
 
 //draws the map in a location, i dont think ill need this later ------ I was right this is just for testing
