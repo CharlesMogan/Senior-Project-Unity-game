74d9233dd493b8c8b42216757f87149b83343fec
CharlesMogan <charlesdmogan@gmail.com>
2017-10-02 02:12:03 -0400

    worked on collisions, they arre not working yet

---
+++ Test Project/Assets/Scripts/PlayerBumper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBumper : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other) {
+       	if(other.gameObject.tag == "Enemy"){
+    		//Destroy(this.gameObject);
+
+    	}
+    }
+
+	void OnCollisionEnter(Collision collision) {
+       	if(collision.gameObject.tag == "Enemy"){
+    		//Destroy(this.gameObject);
+
+    		Vector3 enemyDirection = collision.contacts[0].point - this.transform.position;
+    		rb = collision.gameObject.rigedBody;
+
+    		movement = enemyDirection*100*Time.fixedDeltaTime;
+			rb.MovePosition(rb.position + movement);
+    	}
+    }
+    
+
+
+
+}
