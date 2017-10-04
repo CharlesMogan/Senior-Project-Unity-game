a07f1dcc2e58f9e66e6c05aac72a1dfb7876cff8
Charles Mogan <charlesdmogan@gmail.com>
2017-10-04 18:02:12 -0400

    functional glider gun!!!!!! at 1fp2s

--- Test Project/Assets/GameOfLifeScripts/Conways.cs
+++ Test Project/Assets/GameOfLifeScripts/Conways.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Conways : MonoBehaviour {
 
 	public GameObject cube;
-	bool[,] world = new bool[36,9];
+	bool[,] world = new bool[50,25];
 	float genDelay;
 
 
@@ -13,15 +13,21 @@ public class Conways : MonoBehaviour {
 	void Start () {
 		genDelay = 0;
 		seed();
-		draw();
+		if(world[1,5] == false){
+			Debug.Log("genBad");
+		}
 	}
 
 	void draw(){
+		if(world[1,5] == false){
+			Debug.Log("drawbad");
+		}
+
 		for (int i = 0; i < world.GetLength(0); i++){
 			for (int j = 0; j < world.GetLength(1); j++){
 				if(world[i,j]){
-					GameObject myCube = Instantiate(cube, new Vector3(i+20,-20,j+20), Quaternion.identity);
-					Destroy(myCube,500);
+					GameObject myCube = Instantiate(cube, new Vector3(i,-20,j), Quaternion.identity);
+					Destroy(myCube,1f);
 				}
 			}
 		}
@@ -34,95 +40,124 @@ public class Conways : MonoBehaviour {
 				world[i,j] = false;
 			}
 		}
-		world[24,0] = true;
 		
-		world[22,1] = true;
-		world[24,1] = true;
+		int offset = 4;
+		world[1+offset,5+offset] = true;
+		world[1+offset,4+offset] = true;
+
+		world[2+offset,5+offset] = true;
+		world[2+offset,4+offset] = true;
 		
-		world[12,2] = true;
-		world[13,2] = true;
-		world[20,2] = true;
-		world[21,2] = true;
-		world[34,2] = true;
-		world[35,2] = true;
+		world[11+offset,5+offset] = true;
+		world[11+offset,4+offset] = true;
+		world[11+offset,3+offset] = true;
+
+		world[12+offset,6+offset] = true;
+		world[12+offset,2+offset] = true;
+
+		world[13+offset,7+offset] = true;
+		world[13+offset,1+offset] = true;
+
+		world[14+offset,7+offset] = true;
+		world[14+offset,1+offset] = true;
+
+		world[15+offset,4+offset] = true;
+
+		world[16+offset,6+offset] = true;
+		world[16+offset,2+offset] = true;
+
+		world[17+offset,5+offset] = true;
+		world[17+offset,4+offset] = true;
+		world[17+offset,3+offset] = true;
+
+		world[18+offset,4+offset] = true;
+
+		world[21+offset,7+offset] = true;
+		world[21+offset,6+offset] = true;
+		world[21+offset,5+offset] = true;
+
+		world[22+offset,7+offset] = true;
+		world[22+offset,6+offset] = true;
+		world[22+offset,5+offset] = true;
+
+		world[23+offset,8+offset] = true;
+		world[23+offset,4+offset] = true;
+
+		world[25+offset,9+offset] = true;
+		world[25+offset,8+offset] = true;
+		world[25+offset,4+offset] = true;
+		world[25+offset,3+offset] = true;
 
 
-		world[11,3] = true;
-		world[15,3] = true;
-		world[20,3] = true;
-		world[21,3] = true;
-		world[34,3] = true;
-		world[35,3] = true;
+		world[35+offset,7+offset] = true;
+		world[35+offset,6+offset] = true;
 
-
-		world[0,4] = true;
-		world[1,4] = true;
-		world[10,4] = true;
-		world[16,4] = true;
-		world[20,4] = true;
-		world[21,4] = true;
-		
-
-		world[0,5] = true;
-		world[1,5] = true;
-		world[10,5] = true;
-		world[14,5] = true;
-		world[16,5] = true;
-		world[17,5] = true;
-		world[22,5] = true;
-		world[24,5] = true;
-
-
-
-		world[10,6] = true;
-		world[16,6] = true;
-		world[24,6] = true;
-
-
-		world[11,7] = true;
-		world[15,7] = true;
-
-		world[12,8] = true;
-		world[13,8] = true;
-		
-
+		world[36+offset,7+offset] = true;
+		world[36+offset,6+offset] = true;
 
 	}
 
 
 	void nextGeneration(){
-		bool[,] tempWorld = new bool[36,9];
+		bool[,] tempWorld = new bool[50,25];
 		for (int i = 0; i < world.GetLength(0); i++){
 			for (int j = 0; j < world.GetLength(1); j++){
-				int neighbors = getMooreNeighborhood(i,j);
+				tempWorld[i,j] = world[i,j];
+			}
+		}
+		if(world[1,5] == false){
+			Debug.Log("bad");
+		}
 
+		for (int i = 0; i < world.GetLength(0); i++){
+			for (int j = 0; j < world.GetLength(1); j++){
+				if(world[1,5] == false){
+					Debug.Log("bad for" +i+" "+j);
+				}
+				int neighbors = getMooreNeighborhood(i,j);
+				if(i == 1 && j == 5){
+					Debug.Log("neighbors= "+neighbors);
+				}
 				if(neighbors < 2){
 					tempWorld[i,j] = false;
 				}else if(neighbors > 3){
+					if(world[1,5] == false){
+						Debug.Log("bad3");
+					}
 					tempWorld[i,j] = false;
-				}else if(world[i,j] && (neighbors == 2 || neighbors == 3)){
-					Debug.Log("tacos");
 				}else if(world[i,j] == false && neighbors == 3){
 					tempWorld[i,j] = true;
 				}
 			}
 		}
 		world = tempWorld;
+		if(world[1,5] == false){
+			Debug.Log("bad3");
+		}
 	}
-
+	
 
 
 	int getMooreNeighborhood(int x, int y){
+		if(world[1,5] == false){
+			Debug.Log("moorebad");
+		}
 		int sum = 0;
 		for(int i = -1; i <= 1; i++){
 			for(int j = -1; j <= 1; j++){
-				Debug.Log("tryingTo acsess"+ (x+i)+" "+(y+j));
-				if(x+i>-1 && y+j>-1 && x+i<36 && y+j < 9 && (y!=0 || x!=0)){           ///this is real bad, need to fix
+				//Debug.Log("tryingTo acsess"+ (x+i)+" "+(y+j));
+				if(x+i>-1 && y+j>-1 && x+i<world.GetLength(0) && y+j < world.GetLength(1)){           ///this is real bad, need to fix
 					if(world[x+i,y+j]){
 						sum += 1;
 					}
 				}
 			}
+		}
+		if (world[x,y]){
+			sum-=1;
+		}
+		if(world[1,5] == false){
+			Debug.Log("moore2bad");
 		}
 		return sum;
 	}
@@ -133,9 +168,9 @@ public class Conways : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Time.time> genDelay){
-		//	nextGeneration();
-		//	draw();
-		//	genDelay = Time.time + 2.2f;
+			draw();
+			nextGeneration();
+			genDelay = Time.time + 1f;
 		}
 
 	}
