7decced492a8364f3a78b5eac1efd1f0f0709bba
CharlesMogan <charlesdmogan@gmail.com>
2017-10-10 12:08:40 -0400

    faddffed

--- Test Project/Assets/Scripts/WorldMaker.cs
+++ Test Project/Assets/Scripts/WorldMaker.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldMaker : MonoBehaviour {
+	class world{
+
+	}
+	
+
+
+	class room{
+
+	}
+
+	class cell{
+
+	}
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
